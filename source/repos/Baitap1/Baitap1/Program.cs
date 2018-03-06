c5428c3ab0ab8d1b7d8a4ee2c458828a97c8c1ef
taolaloz12 <taolaloz12@gmail.com>
2018-03-06 21:45:40 +0700

    user

---
+++ source/repos/Baitap1/Baitap1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Baitap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String strHostName = Dns.GetHostName();
+            Console.WriteLine("Host Name: " + strHostName);
+
+            //find host by name
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+
+            //Enumerate IP addresss
+            int nIP = 0;
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                Console.WriteLine("IP #" + ++nIP + ": " + ipaddress.ToString());
+            }
+        }
+    }
+}
